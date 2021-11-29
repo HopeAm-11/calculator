8cfa3f4be9975bba1989715187f0b865c251a603
HopeAm-11 <amalkhalifa11@gmail.com>
2021-11-29 12:26:11 +0400

    added a new function called calculate

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
+let text = document.getElementById("p"); // get the display 
+let numbers = document.getElementsByName("numbers"); // get numbers 
+let operators = document.getElementsByName("operator"); // get operators 
+let equal = document.getElementById("op"); // get operators 
 
-let text = document.getElementById("p");
-text.innerHTML;
-var values = [];
-//let one = document.getElementById("btn-one").value;
-//let two = document.getElementById("btn-two").value;
 
-//document.getElementById("btn-one").addEventListener("click", ()=>{
-//    let one = document.getElementById("btn-one").value;
-///    text.innerHTML = one;
-//});
+let ope = "" // stores the operators 
+let num1 
+let num2
 
-//document.getElementById("btn-two").addEventListener("click", ()=>{
-  //  let two = document.getElementById("btn-two").value;
-  //  text.innerHTML = two;
-//});
 
-//document.getElementById("btn-plus").addEventListener("click", ()=>{
- //   let plus = document.getElementById("btn-plus").value;
-  //  text.innerHTML = plus;
-//});
-
-// Set up a single handler at a common ancestor of all the select elements
-document.body.addEventListener("click", function(event){
-    // event.target references the element that actually triggered the event
-    // Check to see if the event was triggered by a DOM element you care to handle
-    if(event.target.getElementsByTagName("button")){
-      // Access the <p> element that is the previous sibling to the 
-      // select that triggered the event and update it
-      event.target.textContent = event.target.value
-      text.innerHTML= event.target.value;
-      values.push(+text.innerHTML);
-
-    }
-
+// we use the .forEach method to iterate through each button
+numbers.forEach((number) => {
+  // and for each one we add a 'click' listener
+ number.addEventListener('click', (e) => {
+    //alert(button.value);
+    text.innerText += e.target.value
+    //number.value// add singlar value of the button 
   });
+});
 
+operators.forEach((op) => {
+
+  // and for each one we add a 'click' listener
+ op.addEventListener('click', (e) => {
+  if(text.textContent !== ""){
+    num1 = text.textContent
+    ope = op.value
+    text.textContent = ""
+  }
+
+    //alert(button.value);
+    //text.innerHTML += e.target.value
+    //number.value// add singlar value of the button 
+  
+  });
+});
+
+
+// equal section 
+
+// calculat section: 
+
+  
+let calculate = {
+  '+': function(a, b) {
+     return a + b
+  },
+  '<': function(a, b) {
+     return a < b
+  },
+  "*": function(a,b){
+    return a*b
+  }
+};
+
+
+
+
+
+
+// clear buttion 
+
+document.getElementById('btn-clear').addEventListener('click',()=>{
+  text.innerHTML = '';
+})
+
+equal.addEventListener('click',()=>{
+  return console.log(equal.value)
+})
